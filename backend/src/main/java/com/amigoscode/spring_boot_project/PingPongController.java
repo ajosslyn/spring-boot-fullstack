f9fc1f3467cb5fa63b7296463a19003e6b0b2afe
ajosslyn <ajoringi@outlook.com>
2024-10-03 16:31:26 -0500

    update file location and change to trigger action

--- backend/src/main/java/com/amigoscode/spring_boot_project/PingPongController.java
+++ backend/src/main/java/com/amigoscode/spring_boot_project/PingPongController.java
@@ -10,6 +10,6 @@ public class PingPongController {
 
     @GetMapping("/ping")
     public PingPong getPingPong() {
-        return new PingPong("PONG ");
+        return new PingPong("pong ");
     }
 }
