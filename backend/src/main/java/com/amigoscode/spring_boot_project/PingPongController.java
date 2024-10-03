8017451883e0ef4189a2e47ee18f0e44719cfaf9
ajosslyn <ajoringi@outlook.com>
2024-10-03 13:31:41 -0500

    change to trigger action

--- backend/src/main/java/com/amigoscode/spring_boot_project/PingPongController.java
+++ backend/src/main/java/com/amigoscode/spring_boot_project/PingPongController.java
@@ -6,12 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PingPongController {
 
-    private static int COUNTER = 0;
-
     record PingPong(String result) {}
 
     @GetMapping("/ping")
     public PingPong getPingPong() {
-        return new PingPong("Pong: %s".formatted(++COUNTER));
+        return new PingPong("PONG ");
     }
 }
